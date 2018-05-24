4081972846837636db3cece2632fb702a8675371
unknown <cctpmax@gmail.com>
2018-05-24 19:26:56 -0400

    Wrote the remainder of the function

--- TimeConvert/MinutesToHours.ps1
+++ TimeConvert/MinutesToHours.ps1
@@ -1,16 +1,29 @@
 [CmdletBinding()]
 Param (
 
+    #A mandatory parameter of the integer type, that is mandatory and accepts pipeline input
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+    [Int32]$Minutes
+
 )
 
-Begin {
-
-}
-
+#ForEach object piped in
 Process {
 
-}
+    #Divide the number of minutes by 60 and round down to determine the number of whole hours
+    $Hours = [math]::Truncate($Minutes / 60)
 
-End {
-    
+    #Determine the remainder number of minutes by doing minutes modulos 60
+    $RemainderMinutes = $Minutes % 60
+
+    #Construct an object with three properties: the original input, the number of whole hours, and the remainder hours
+    $Obj = [PSCustomObject] @{
+        Input = $Minutes
+        Hours = $Hours
+        Minutes = $RemainderMinutes
+    }
+
+    #Output the object
+    Write-Output $Obj
+
 }
